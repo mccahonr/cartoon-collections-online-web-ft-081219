4b8e7172966ca64679ecc9cdcb9f57fd3782ad23
Ryan McCahon <mccahonr@yahoo.com>
2019-08-16 14:26:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,10 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index {|name, number| puts "#{number +1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |element|
+    "#{element.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
