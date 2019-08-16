8d359a3c3ae00f418d2ccb03778004e0ea1c55b7
Ryan McCahon <mccahonr@yahoo.com>
2019-08-16 14:32:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(array)# code an argument here
   array.any?{|i| i.length > 4}# Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types[i])
 end
