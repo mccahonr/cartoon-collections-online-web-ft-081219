bda34c0538b59ce2c85bd91919f034e536eaeed5
Ryan McCahon <mccahonr@yahoo.com>
2019-08-16 14:26:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,6 +7,7 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
   array.collect do |element|
     "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
